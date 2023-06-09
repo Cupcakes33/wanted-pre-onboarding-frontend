15e7072e5c99947683704ff3b7235e337b7b3a27
Cupcakes33 <rute930627@gmail.com>
2023-06-09 16:49:58 +0900

    feat: axios instance 골격 추가

---
+++ src/service/instance.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { tokenManager } from "../util/tokenManager";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER,
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers["Authorization"] = tokenManager.getToken();
+  return config;
+});
+

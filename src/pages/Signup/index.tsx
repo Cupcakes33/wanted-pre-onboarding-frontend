8a2ad0be04ee66117229dd4c2b669cb1c38e2759
Cupcakes33 <rute930627@gmail.com>
2023-06-10 22:29:19 +0900

    refector: form 로직을 커스텀 훅으로 정리, alertText 를 helpers 에 추가

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,64 +1,25 @@
-import { useEffect, useState } from "react";
+import { alertText } from "../../helpers/alertText";
+import useForm from "../../hooks/useForm";
 import { signup } from "../../service/sign";
 import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
-  const [form, setForm] = useState({ email: "", password: "" });
-  const [isValidate, setIsValidate] = useState(true);
-  const [errorText, setErrorText] = useState({ email: "", password: "" });
-  const [isTouched, setIsTouched] = useState({ email: false, password: false });
-
   const navigate = useNavigate();
-  const { email, password } = form;
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsTouched((prev) => ({ ...prev, [e.target.name]: true }));
-    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+  const { form, handleChange, isValidate, errorText } = useForm();
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    signup(email, password)
+    signup(form.email, form.password)
       .then((res) => res.status === 201 && navigate("/signin"))
-      .catch();
+      .catch(() => alert(alertText.SIGNUP[400]));
   };
-
-  useEffect(() => {
-    setErrorText({ email: "", password: "" });
-    if (isTouched.email && (email === "@" || !email.includes("@"))) {
-      setErrorText((prev) => ({
-        ...prev,
-        email: "Email 에는 @ 기호가 포함되어야 합니다.",
-      }));
-    }
-
-    if (isTouched.password && password.length < 8) {
-      setErrorText((prev) => ({
-        ...prev,
-        password: "password 는 8글자 이상이어야 합니다.",
-      }));
-    }
-  }, [email, password]);
-
-  useEffect(() => {
-    if (
-      isTouched.email &&
-      isTouched.password &&
-      errorText.email === "" &&
-      errorText.password === ""
-    ) {
-      setIsValidate(false);
-    } else {
-      setIsValidate(true);
-    }
-  }, [errorText]);
 
   return (
     <>
       <div className="bg-gray-200 w-[450px] rounded-3xl flex flex-col p-5 items-center">
         <h3 className="text-black text-2xl mb-3">" 회원가입 페이지 "</h3>
         <form className="flex flex-col gap-3 w-full" onSubmit={handleSubmit}>
-          <div className="relative">
+          <div>
             <input
               name="email"
               className="w-full h-10 rounded-3xl px-5"

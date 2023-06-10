864ef0338c38279eab981c3510851cfd9101d951
Cupcakes33 <rute930627@gmail.com>
2023-06-10 16:41:48 +0900

    fix: Signin, Signup 컴포넌트 분리

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,49 +1,50 @@
 import { useState } from "react";
+import { signin } from "../../service/sign";
+import { tokenManager } from "../../util/tokenManager";
 
 export default function Signin() {
   const [form, setForm] = useState({ email: "", password: "" });
+  const { email, password } = form;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    console.log(form);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    console.log(form);
+    const response = await signin(email, password);
+    tokenManager.setToken(response.data.access_token);
   };
 
   return (
     <>
-      <div className="w-screen h-screen bg-neutral-100 flex justify-center items-center">
-        <div className="bg-gray-200 w-[450px] h-[300px] rounded-3xl">
-          <form
-            className="flex flex-col p-5 gap-3 h-full justify-center"
-            onSubmit={handleSubmit}
+      <div className="bg-gray-200 w-[450px] rounded-3xl flex flex-col p-5 items-center">
+        <h3 className="text-black text-2xl mb-3">" 로그인 페이지 "</h3>
+        <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit}>
+          <input
+            name="email"
+            className="w-full h-10 rounded-3xl px-5"
+            data-testid="email-input"
+            onChange={handleChange}
+            value={form.email}
+          />
+          <input
+            name="password"
+            type="password"
+            className="w-full h-10 rounded-3xl px-5"
+            data-testid="password-input"
+            onChange={handleChange}
+            value={form.password}
+          />
+          <button
+            className="w-full h-10 bg-black text-white rounded-3xl"
+            data-testid="signup-button"
+            type="button"
+            onClick={handleSubmit}
           >
-            <input
-              name="email"
-              className="w-full h-10 rounded-3xl px-5"
-              data-testid="email-input"
-              onChange={handleChange}
-              value={form.email}
-            />
-            <input
-              name="password"
-              type="password"
-              className="w-full h-10 rounded-3xl px-5"
-              data-testid="password-input"
-              onChange={handleChange}
-              value={form.password}
-            />
-            <button
-              className="w-full h-10 bg-black text-white rounded-3xl"
-              data-testid="signup-button"
-            >
-              회원가입
-            </button>
-          </form>
-        </div>
+            로그인
+          </button>
+        </form>
       </div>
     </>
   );
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { useState } from "react";
+import { signup } from "../../service/sign";
 
 export default function Signup() {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const { email, password } = form;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    signup(email, password).then((res) => console.log(res));
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <>
+      <div className="bg-gray-200 w-[450px] rounded-3xl flex flex-col p-5 items-center">
+        <h3 className="text-black text-2xl mb-3">" 회원가입 페이지 "</h3>
+        <form className="flex flex-col gap-3 w-full" onSubmit={handleSubmit}>
+          <input
+            name="email"
+            className="w-full h-10 rounded-3xl px-5"
+            data-testid="email-input"
+            onChange={handleChange}
+            value={form.email}
+          />
+          <input
+            name="password"
+            type="password"
+            className="w-full h-10 rounded-3xl px-5"
+            data-testid="password-input"
+            onChange={handleChange}
+            value={form.password}
+          />
+          <button
+            className="w-full h-10 bg-black text-white rounded-3xl"
+            data-testid="signup-button"
+          >
+            회원가입
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
